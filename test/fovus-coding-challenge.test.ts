c8e0fbbba2414eef6fd7420c26ce8f26588bf113
Devansh Sehgal <devanshsehgal02@gmail.com>
2024-05-27 10:11:51 +0530

    Initial commit

---
+++ test/fovus-coding-challenge.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as FovusCodingChallenge from '../lib/fovus-coding-challenge-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/fovus-coding-challenge-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new FovusCodingChallenge.FovusCodingChallengeStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
